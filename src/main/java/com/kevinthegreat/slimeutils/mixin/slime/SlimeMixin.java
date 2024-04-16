6ac7c768216900df1d7c2ceaf088a9ec48814818
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-04-16 14:47:23 -0400

    Make SlimeMixin fail soft

--- src/main/java/com/kevinthegreat/slimeutils/mixin/slime/SlimeMixin.java
+++ src/main/java/com/kevinthegreat/slimeutils/mixin/slime/SlimeMixin.java
@@ -16,7 +16,8 @@ import java.util.Random;
 public class SlimeMixin {
     @Redirect(method = "lambda$onInitialize$2",
             slice = @Slice(from = @At(value = "INVOKE", target = "Ljava/util/Random;<init>(J)V")),
-            at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I")
+            at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"),
+            require = 0
     )
     private static int slimeutils$modifyCanSpawn(Random random, int bound, @Local(ordinal = 0) int x, @Local(ordinal = 1) int z, @Local long seed, @Local(argsOnly = true) CommandContext<ServerCommandSource> context) {
         return SlimeUtils.isSlimeChunk(x, z, seed, context.getSource().getWorld()) ? 0 : 1;
